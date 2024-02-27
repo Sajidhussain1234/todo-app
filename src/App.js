1cf45afe3bf1eab2bfbfb44dabcc8791dffeb283
Sajid Hussain <sajidmcsbwn@gmail.com>
2024-02-27 12:44:03 +0500

    private route protection implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import TodoUI from "./components/TodoUI";
-import TodoList from "./components/TodoList";
 import Login from "./pages/Login";
 import SignUp from "./pages/Signup";
+import TodoList from "./components/TodoList";
+import { useSelector } from "react-redux";
+
+// Custom Route component for authentication
+const ProtectedRoute = ({ element, ...props }) => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  return isAuthenticated ? (
+    element
+  ) : (
+    <Navigate to="/login" replace state={{ from: props.location }} />
+  );
+};
 
 function App() {
   return (
@@ -12,11 +24,15 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<TodoUI />} />
-        <Route path="/home" element={<TodoList />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
+
+        {/* Private Routes */}
+        <Route
+          path="/home"
+          element={<ProtectedRoute element={<TodoList />} />}
+        />
       </Routes>
-      <Outlet />
     </BrowserRouter>
   );
 }
